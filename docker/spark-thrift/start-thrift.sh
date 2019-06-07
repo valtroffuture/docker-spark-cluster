8649c0cca4075ff156b6d998d2d21b214eac4690
Valeriy Trofimov <ValTrofFuture@gmail.com>
2019-06-07 01:08:33 +0000

    latest code

--- docker/spark-thrift/start-thrift.sh
+++ docker/spark-thrift/start-thrift.sh
@@ -3,10 +3,14 @@
 . "/spark/sbin/spark-config.sh"
 . "/spark/bin/load-spark-env.sh"
 
-mkdir -p $SPARK_WORKER_LOG
+mkdir -p $SPARK_THRIFT_LOG
 
 export SPARK_HOME=/spark
 
-ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
+ln -sf /dev/stdout $SPARK_THRIFT_LOG/spark-thrift.out
 
-/spark/sbin/start-thriftserver.sh >> $SPARK_WORKER_LOG/spark-worker.out
+/spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_THRIFT_WEBUI_PORT $SPARK_MASTER >> $SPARK_THRIFT_LOG/spark-thrift.out
+
+#/spark/sbin/start-thriftserver.sh
+
+#/spark/sbin/start-thriftserver.sh
