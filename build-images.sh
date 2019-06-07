65df07c7bd37d0ec48da5fbfca76181c77fc0c4a
Valeriy Trofimov <ValTrofFuture@gmail.com>
2019-06-07 00:26:47 +0000

    add thrift worker

--- build-images.sh
+++ build-images.sh
@@ -5,4 +5,5 @@ set -e
 docker build -t spark-base:2.3.1 ./docker/base
 docker build -t spark-master:2.3.1 ./docker/spark-master
 docker build -t spark-worker:2.3.1 ./docker/spark-worker
+docker build -t spark-thrift:2.3.1 ./docker/spark-thrift
 docker build -t spark-submit:2.3.1 ./docker/spark-submit
