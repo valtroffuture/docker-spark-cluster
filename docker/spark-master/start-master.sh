12021de84e4bfe7ce031ea8593120eeb0cd01759
Val Trofimov <ValTrofFuture@github.com>
2019-06-13 15:25:11 +0000

    fix thrift rsync error

--- docker/spark-master/start-master.sh
+++ docker/spark-master/start-master.sh
@@ -13,3 +13,6 @@ export SPARK_HOME=/spark
 ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 
 cd /spark/bin && /spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
+
+
+
